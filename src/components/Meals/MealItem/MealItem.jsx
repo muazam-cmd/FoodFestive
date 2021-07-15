434bde57370fc73b6f7171b6c94a028c89312cc4
muazam-cmd <63458163+muazam-cmd@users.noreply.github.com>
2021-07-15 18:07:44 +0500

    Add files via upload

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm';
+import CartContext from '../../../Store/cart-context';
+
+const MealItem = (props) =>{
+   const cartCtx= useContext(CartContext);
+    const addToCartHandler = amount =>  {
+        CartContext.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    };
+    
+    return (
+        <li className={classes.meal}>
+            <div><h3>{props.name}</h3>
+            <div className={classes.description}>{props.description}</div>
+            <div className={classes.price}>${props.price}</div>
+            </div>
+            <div> 
+                <MealItemForm onAddToCart={addToCartHandler}/>
+            </div>
+        </li>
+    );
+
+}
+export default MealItem;
